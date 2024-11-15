9475c10b348f14f80a7b542ed2d364e9bf385224
Efestus31 <biagiohd@gmail.com>
2024-11-15 16:26:10 +0100

    add html structure add function handle click

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-
+import languages from './data/languages.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [active, setActive] = useState(0)
 
+  function handleClick(e) {
+    const newActive = Number(e.target.getAttribute('data-index'))
 
+    setActive(newActive)
+  }
 
 
 
@@ -13,6 +17,19 @@ function App() {
 
   return (
     <>
+      <h1>Learn Web development</h1>
+      <div className="container">
+        {languages.map((item, index) => (
+          <div className="pill-item" key={item.id}>
+            <button onClick={handleClick} data-index={index}>{item.title}</button>
+            <div className={active == index ? 'active' : 'hide'}>
+              {item.description}
+            </div>
+          </div>
+        ))}
+      </div>
+
+
     </>
   )
 }
